5987ad46054161999b6ebab0e59e158d1828ad8c
Joshua Johnson <me@josh.land>
2013-11-18 16:12:53 -0800

    added e-mail list to cherrypy - testing functionality.

--- pyDownloader.py
+++ pyDownloader.py
@@ -15,10 +15,12 @@ class HelloWorld(object):
     def index(self):
         #host = cherrypy.request.headers['Host']
         current_emails = emailCrawl.listAllEmails()
-        builtPage = "Inbox Downloads To Queue: \n\n"
+        builtPage = "<!DOCTYPE html> <html> <body> <h4> Inbox Downloads To Queue:</h4><ul>"
 
         for download_url in current_emails:
-            builtPage = builtPage + str(download_url) + "\n"
+            builtPage = builtPage + "<li>" + str(download_url) + "</li>"
+
+        builtPage = builtPage + "</ul></body></html>"
 
         return (builtPage)
     #index.exposed = True
