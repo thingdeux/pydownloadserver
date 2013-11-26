0523daa7c1369ff05d18ebde5148759d9b462cc2
Jason Yates <JLYates1985@gmail.com>
2013-11-26 13:47:37 -0800

    Added support to rename file if duplicate is found

--- myDownload.py
+++ myDownload.py
@@ -10,6 +10,7 @@ import urllib2
 import threading
 import logger
 import database
+import os
 
 class myDownload(threading.Thread):
     def __init__(self, url, pathToSave, filename, threadID, sema):
@@ -18,6 +19,7 @@ class myDownload(threading.Thread):
         self.threadID = threadID
         self.url = url
         self.filename = filename
+        self.pathToSave = pathToSave
         self.location_to_save = pathToSave + filename
         self.progress = None
         self.response = None
@@ -29,6 +31,20 @@ class myDownload(threading.Thread):
     def __getResponsecode(self):
         code = self.response.code
         return code
+
+    def getProgress(self):
+        try:
+            total_size = self.response.info().getheader('Content-Length').strip()
+            total_size = int(total_size)
+            bytes_so_far = 0
+            chunk_size = 8192
+            chunk = self.response.read(chunk_size)
+            bytes_so_far += len(chunk)
+            percent = float(bytes_so_far) / total_size
+            percent = round(percent * 100, 2)
+            return percent
+        except:
+            logger.log ("error retrieving progress")
 
     def run(self):
         #Aquire a lock based on number of allowed concurrent processes
@@ -47,6 +63,12 @@ class myDownload(threading.Thread):
             #The following breaks down any downloading into memory manageable 32MB chunks.
             downloadable_chunk_size = 16*32768
 
+            ##check if file name already exists, if so, add a number
+            somenumber = 1
+            while os.path.isfile(self.location_to_save):
+                self.location_to_save = self.pathToSave + "(" + str(somenumber) + ")" + self.filename
+                somenumber = somenumber + 1
+
             #open the file and save
             database.updateJobStatus(int(self.threadID), "downloading")
             with open(self.location_to_save, 'wb') as file_object:
@@ -62,18 +84,3 @@ class myDownload(threading.Thread):
             database.updateJobStatus(int(self.threadID), "failed") #Update status to failed
             #following releases the lock so we can fire the next download
             self.__releaseAndExit()
-
-    def getProgress(self):
-        try:
-            total_size = self.response.info().getheader('Content-Length').strip()
-            total_size = int(total_size)
-            bytes_so_far = 0
-            chunk_size = 8192
-            chunk = self.response.read(chunk_size)
-            bytes_so_far += len(chunk)
-            percent = float(bytes_so_far) / total_size
-            percent = round(percent * 100, 2)
-            return percent
-        except:
-            logger.log ("error retrieving progress")
-
