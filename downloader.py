4cc0ff6f58653acc02de37a00e51660c6a15e041
Jason Yates <JLYates1985@gmail.com>
2013-11-25 16:37:28 -0800

    removed some old test code no longer needed

--- downloader.py
+++ downloader.py
@@ -24,49 +24,6 @@ def queueDownload(url, source="web"):
 			logger.log("Unable to queue url - " + error)
 
 
-def testFunction():
-
-	#STATIC TEST DATA=======
-	test_url1 = "http://netstorage.unity3d.com/unity/UnitySetup-4.3.0.exe"
-	test_url2 = "http://www.google.com"
-	test_filename1 = test_url1.split('/')[-1]
-	test_filename2 = test_url2.split('/')[-1]
-	#=======================
-
-	#Creates 2 thread objects with the myDownload class.
-	thread1 = myDownload.myDownload(test_url1, test_filename1)
-	#thread2 = myDownload.myDownload(test_url2, test_filename2)
-	#starts my test threads
-	thread1.start()
-	#thread2.start()	
-
-	##tests the get process functions
-	#print thread1.filename
-	#thread1.getProgress()
-	#print thread2.filename
-	#thread2.getProgress()
-
-
-
-class aTestThread(threading.Thread):
-	def __init__(self, threadID, name, counter):
-		threading.Thread.__init__(self) #Overriding default init
-		self.threadID = threadID
-		self.name = name
-		self.counter = counter		
-
-	def run(self):
-		for i in range(100):
-			print (str(i)),
-		print("")
-def testThreading():
-	thread1 = aThread(1, "Uno", 1)
-	thread2 = aThread(2, "Uno", 2)
-
-	thread1.start()
-	thread2.start()
-
-
 def makeDownloadFileName(url):
     downloadFileName = url.split('/')[-1]
     return downloadFileName
@@ -118,19 +75,4 @@ def manageQueues():
             t.start()
 
 
-
-
-
-            #print "sent start command"
-            #if t.isAlive == True:
-            #    print str(t.threadID) + "is alive"
-            #else:
-            #    print str(t.threadID) + "Terminated"
-
-
-
-
 #manageQueues()
-
-#testFunction()
-#testThreading()
