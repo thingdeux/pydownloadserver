ec40da19254d538969b7d671f6dc25b9a0b509d2
Jason Yates <JLYates1985@gmail.com>
2013-12-03 14:14:42 -0800

    Think I have this stupid time left working correctly, along with the average download speed

--- myDownload.py
+++ myDownload.py
@@ -37,14 +37,17 @@ class myDownload(threading.Thread):
         return code
 
     def __getAverageSpeed(self, list_of_speeds):
-        ## Here for possible use later -- Takes list of speeds and returns average speeds in
-        ## bytes per second
+        ## speeds come in at a unit of bit per microsecond
+        ## 
         number_of_items = 0
-        sum_of_speeds = 0
+        sum_of_speeds = float(0)
         for speed in list_of_speeds:
             number_of_items = number_of_items + 1
-            sum_of_speeds = sum_of_speeds + float(speed)
-            average_speed = sum_of_speeds/number_of_items
+            sum_of_speeds = sum_of_speeds + speed
+            # print speed
+        average_speed = sum_of_speeds/number_of_items
+        # average_speed = (average_speed*1000000)/1024 #convert to kb
+        #"{0:.2f}".format(
         return average_speed
 
     def getSpeed(self, chunk_size, date_time_start_of_chunk, date_time_end_of_chunk):
@@ -54,8 +57,9 @@ class myDownload(threading.Thread):
 
     def getTimeRemaining(self, bytes_so_far, speed):
         total_size = self.response.info().getheader('Content-Length').strip()
-        amount_remaining = int(total_size)-int(bytes_so_far)
-        return (speed*amount_remaining)/1000000
+        amount_remaining = long(total_size)-long(bytes_so_far)
+        # print str(amount_remaining)
+        return (amount_remaining/speed)/100000
 
     def run(self):
         #Aquire a lock based on number of allowed concurrent processes
@@ -79,7 +83,7 @@ class myDownload(threading.Thread):
 
             ##Update the database to let everyone know we're now in downloading status
             database.updateJobStatus(int(self.threadID), "downloading")
-
+            chunk_speeds = []
             chunk_loop_number = 0
             bytes_so_far = 0
             with open(self.location_to_save, 'wb') as file_object:
@@ -91,26 +95,35 @@ class myDownload(threading.Thread):
                     if not chunk: break
                     date_time_end_of_chunk = datetime.datetime.now()
                     chunk_download_speed = self.getSpeed(len(chunk), date_time_start_of_chunk, date_time_end_of_chunk)
-                    
+
                     date_time_start_of_write = datetime.datetime.now()
                     file_object.write(chunk)
                     date_time_end_of_write = datetime.datetime.now()
                     chunk_write_speed = self.getSpeed(len(chunk), date_time_start_of_write, date_time_end_of_write)
 
+                    bytes_so_far += len(chunk)
+
                     #try and find optimal chunking speed
                     if chunk_write_speed < chunk_download_speed:
                         self.chunk_variable_size = self.chunk_variable_size + .2
 
-                    bytes_so_far += len(chunk)
-                    
-                    time_remaining = self.getTimeRemaining(bytes_so_far, chunk_download_speed)
-                    
-                    #Write time remaining & download speed once every 5 chunks to help reduce excessive database writes. 
-                    if chunk_loop_number == 5:
+                    #Write time remaining & download speed once every 20 chunks to help reduce excessive database writes. 
+                    if chunk_loop_number == 30:
+                        reported_download_speed = self.__getAverageSpeed(chunk_speeds)
+                        time_remaining = self.getTimeRemaining(bytes_so_far, reported_download_speed) #(reported_download_speed*1024)/1000000)
                         reported_time_remaining = "{0:.2f}".format(time_remaining) #time remaining in seconds
-                        reported_download_speed = str(int((chunk_download_speed*1000000)/1024)) #speed in KB/s
 
+                        database_update_string = str(reported_time_remaining + " Sec @ " + "{0:.2f}".format((reported_download_speed/1024)*100000) + " KB/s")
+                        database.updateTimeRemainingByID(self.threadID, database_update_string)
+                        # print(str(reported_time_remaining) + " Sec @ " + str("{0:.2f}".format((reported_download_speed/1024)*100000) + " Kb/s"))
+                        # print(str(self.response.info().getheader('Content-Length').strip()) + " total size")
+                        # print(str(bytes_so_far) + " written")
+                        chunk_loop_number = 0
                     chunk_loop_number = chunk_loop_number + 1
+                    date_time_end_of_loop = datetime.datetime.now()
+                    Total_Loop_Speed = self.getSpeed(len(chunk), date_time_start_of_chunk, date_time_end_of_loop)
+                    chunk_speeds.append(Total_Loop_Speed)
+
             database.updateJobStatus(int(self.threadID), "successful")
             #following releases the lock so we can fire the next download
             self.__releaseAndExit()
